e8eb5a21b129d0f6bb68f89d6e2859ee0e9e6bbf
Matthew Lee <laughew@gmail.com>
2018-07-18 12:53:27 -0700

    add a few more improvements to organization in views.cljs

--- src/cljs/sonic/views.cljs
+++ src/cljs/sonic/views.cljs
@@ -4,15 +4,19 @@
    [sonic.subs :as subs]
    [sonic.events :as events]))
 
+(defn actionDisabled?
+  [text requiredSystem]
+  (if (or (and @(rf/subscribe [:firing?]) (not= text "Fire")) 
+          (= @(rf/subscribe [:turn]) 1)
+          (events/systemDisabled? requiredSystem :playerShip))
+      true
+      false))
+
 (defn actionButton
   [requiredSystem event text]
   (fn []
     [:button.action {:on-click (events/actionDispatch event)
-                     :disabled (if (or (and @(rf/subscribe [:firing?]) (not= text "Fire")) 
-                                       (= @(rf/subscribe [:turn]) 1)
-                                       (events/systemDisabled? requiredSystem :playerShip))
-                                 true
-                                 false)}
+                     :disabled (actionDisabled? text requiredSystem)} 
                     text]))
                                  
 (defn shipVitalityDisplay
@@ -33,7 +37,8 @@
                      (get 0))]
     [:button.system
      (if (= type :playerShip)
-       {:disabled (or firing? (events/systemDisabled? system type))}
+       {:disabled (or firing? 
+                      (events/systemDisabled? system type))}
        (if firing?
          {:on-click (events/damageDispatch system type)}
          {:on-click (fn [] (rf/dispatch [:doNothing]))}))
@@ -53,7 +58,6 @@
         firingMode (if (= @(rf/subscribe [:firing?]) false)
                      "Inactive"
                      "Active")]
-    [:div
      [:div.flexContainer
       [:div.infoDisplayArea 
        [:textarea.infoDisplay {:value (str turn "'s Turn")
@@ -84,6 +88,6 @@
       [:div.actionBar
        [actionButton :weapons :actionFire "Fire"]
        [actionButton :shields :actionChargeShields "Charge Shields"]
-       [actionButton :engines :actionFlee "Flee"]]]]))
+       [actionButton :engines :actionFlee "Flee"]]]))
       
      
