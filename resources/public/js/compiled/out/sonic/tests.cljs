43faac2e8fcd4b405bd59db36da07d86ca521343
Kevin <kevinye88@gmail.com>
2018-07-23 16:07:40 -0700

    implemented tests for depleted and reducced shields

--- resources/public/js/compiled/out/sonic/tests.cljs
+++ resources/public/js/compiled/out/sonic/tests.cljs
@@ -46,4 +46,83 @@
 
   )
 
+(deftest moreEvents
+  "tests with reduced player health and shields"
+  (let [sim-db {:playerShip {:systems {:weapons [2 1]
+                                   :shields [2 1]
+                                   :engines [2 1]}
+                             :HP 50
+                             :shields 50}
+                :enemyShip {:systems {:weapons [2 1]
+                                      :shields [2 1]
+                                      :engines [2 1]}
+                            :HP 50
+                            :shields 50}}]
+    (is (= 40 (events/calcShieldsStrength (-> sim-db :playerShip :systems :shields second) 5)))
+    (is (= 40 (-> (events/newHP [(:playerShip sim-db)
+                                  (:enemyShip sim-db)
+                                   :weapons
+                                    60])
+                  (get 0)
+                  (:HP))))
+    (is (= 0 (-> (events/newShields [(:playerShip sim-db)
+                                      (:enemyShip sim-db)
+                                       :weapons
+                                        60])
+                (get 0)
+                (:shields))))
+    (is (= [2 1] (-> (events/newSystemHP [(:playerShip sim-db)
+                                          (:enemyShip sim-db)
+                                           :weapons
+                                            60])
+                     (get 0)
+                     (:systems)
+                     (:weapons))))
+    (is (= 58 (-> (events/chargeShields (:playerShip sim-db) 1)
+                (:shields))))
+  ))
+
+(deftest evenMoreEvents
+  "tests with depleted player shields"
+  (let [sim-db {:playerShip {:systems {:weapons [2 1]
+                                   :shields [2 1]
+                                   :engines [2 1]}
+                             :HP 80
+                             :shields 0}
+                :enemyShip {:systems {:weapons [2 1]
+                                      :shields [2 1]
+                                      :engines [2 1]}
+                            :HP 50
+                            :shields 50}}]
+    (is (= 20 (-> (events/newHP [(:playerShip sim-db)
+                                  (:enemyShip sim-db)
+                                   :weapons
+                                    60])
+                  (get 0)
+                  (:HP))))
+    (is (= 0 (-> (events/newShields [(:playerShip sim-db)
+                                      (:enemyShip sim-db)
+                                       :weapons
+                                        60])
+                 (get 0)
+                 (:shields))))
+    (is (= [1 1] (-> (events/newSystemHP [(:playerShip sim-db)
+                                          (:enemyShip sim-db)
+                                           :weapons
+                                            60])
+                     (get 0)
+                     (:systems)
+                     (:weapons))))
+    (is (= 16 (-> (events/chargeShields (:playerShip sim-db) 2)
+                  (:shields))))
+
+
+
+
+
+
+
+  ))
+
+
 (run-tests)
