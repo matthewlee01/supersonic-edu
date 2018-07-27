15526f26ad7f02bb68fdcedf010974e0f0f8a72b
Kevin <kevinye88@gmail.com>
2018-07-27 09:47:51 -0700

    adjusted indentation in tests file

--- src/cljs/sonic/tests.cljs
+++ src/cljs/sonic/tests.cljs
@@ -1,24 +1,24 @@
 (ns sonic.tests
- (:require
-  [reagent.core :as r]
-  [re-frame.core :as rf]
-  [sonic.events :as events]
-  [sonic.views :as views]
-  [sonic.config :as config]
-  [sonic.db :as db]
-  [sonic.core :as core]
-  [cljs.test :refer-macros [deftest is testing run-tests]]))
+  (:require
+   [reagent.core :as r]
+   [re-frame.core :as rf]
+   [sonic.events :as events]
+   [sonic.views :as views]
+   [sonic.config :as config]
+   [sonic.db :as db]
+   [sonic.core :as core]
+   [cljs.test :refer-macros [deftest is testing run-tests]]))
 
 (deftest calc
   "test for functions that do simple calculations"
   (let [testRoll (events/diceRoll)
         sim-db db/default-db]
     (is (= true (-> sim-db
-                  (events/toggleFiringMode nil)
-                  (:firing?))))
+                    (events/toggleFiringMode nil)
+                    (:firing?))))
     (is (= true (-> sim-db
-                  (events/toggleRepairingMode nil)
-                  (:repairing?))))
+                    (events/toggleRepairingMode nil)
+                    (:repairing?))))
     (is (= true (and (> testRoll 0)
                      (< testRoll 7))))
     (is (= 40 (events/calcLaserDamage 1 4)))
@@ -26,8 +26,8 @@
     (is (= 115 (events/calcShieldsMax 2)))
     (is (= 120 (events/calcShieldsStrength 3 5)))
     (is (= 64 (events/calcRepairStrength 4 4)))
-    (is (= [4 3] (events/createRepairedSystem 3)))
-  ))
+    (is (= [4 3] (events/createRepairedSystem 3)))))
+
 
 
 
@@ -39,47 +39,47 @@
         enemyShip (:enemyShip sim-db)
         laserAttackInit [playerShip enemyShip :engines 40 :lasers]
         laserAttackFinal (-> laserAttackInit
-                           (events/newHP)
-                           (events/newShields)
-                           (events/newSystemHP))
-        missileAttackInit [playerShip enemyShip :engines 40 :missiles]
-        missileAttackFinal (-> missileAttackInit
                              (events/newHP)
                              (events/newShields)
-                             (events/newSystemHP))]
+                             (events/newSystemHP))
+        missileAttackInit [playerShip enemyShip :engines 40 :missiles]
+        missileAttackFinal (-> missileAttackInit
+                               (events/newHP)
+                               (events/newShields)
+                               (events/newSystemHP))]
     ;--------laser attack calculations--------
 
-    ;checks shields of defender   
+    ;checks shields of defender
     (is (= 60 (-> laserAttackFinal
-                (get 0)
-                (:shields))))
+                  (get 0)
+                  (:shields))))
     ;checks HP of defender
     (is (= 100 (-> laserAttackFinal
-                 (get 0)
-                 (:HP))))
+                   (get 0)
+                   (:HP))))
     ;checks targeted system of defender
     (is (= 2 (-> laserAttackFinal
-               (get 0)
-               (:systems)
-               (:engines)
-               (get 0))))
+                 (get 0)
+                 (:systems)
+                 (:engines)
+                 (get 0))))
 
     ;---------missile attack calculations------
 
-    ;checks shields of defender   
+    ;checks shields of defender
     (is (= 100 (-> missileAttackFinal
-                 (get 0)
-                 (:shields))))
+                   (get 0)
+                   (:shields))))
     ;checks HP of defender
     (is (= 60 (-> missileAttackFinal
-                (get 0)
-                (:HP))))
+                  (get 0)
+                  (:HP))))
     ;checks systems of defender
     (is (= 1 (-> missileAttackFinal
-               (get 0)
-               (:systems)
-               (:engines)
-               (get 0))))))
+                 (get 0)
+                 (:systems)
+                 (:engines)
+                 (get 0))))))
 
 
 (deftest reducedShields
@@ -106,45 +106,45 @@
         enemyShip (:enemyShip sim-db)
         laserAttackInit [playerShip enemyShip :engines 40 :lasers]
         laserAttackFinal (-> laserAttackInit
-                           (events/newHP)
-                           (events/newShields)
-                           (events/newSystemHP))
-        missileAttackInit [playerShip enemyShip :engines 40 :missiles]
-        missileAttackFinal (-> missileAttackInit
                              (events/newHP)
                              (events/newShields)
-                             (events/newSystemHP))]
+                             (events/newSystemHP))
+        missileAttackInit [playerShip enemyShip :engines 40 :missiles]
+        missileAttackFinal (-> missileAttackInit
+                               (events/newHP)
+                               (events/newShields)
+                               (events/newSystemHP))]
     ;--------laser attack calculations--------
 
-    ;checks shields of defender   
+    ;checks shields of defender
     (is (= 0 (-> laserAttackFinal
-               (get 0)
-               (:shields))))
+                 (get 0)
+                 (:shields))))
     ;checks HP of defender
     (is (= 80 (-> laserAttackFinal
-                (get 0)
-                (:HP))))
+                  (get 0)
+                  (:HP))))
     ;checks targeted system of defender
     (is (= [1 1] (-> laserAttackFinal
-                   (get 0)
-                   (:systems)
-                   (:engines))))
+                     (get 0)
+                     (:systems)
+                     (:engines))))
 
     ;---------missile attack calculations------
 
-    ;checks shields of defender   
+    ;checks shields of defender
     (is (= 20 (-> missileAttackFinal
-                (get 0)
-                (:shields))))
+                  (get 0)
+                  (:shields))))
     ;checks HP of defender
     (is (= 60 (-> missileAttackFinal
-                (get 0)
-                (:HP))))
+                  (get 0)
+                  (:HP))))
     ;checks systems of defender
     (is (= [1 1] (-> missileAttackFinal
-                   (get 0)
-                   (:systems)
-                   (:engines))))))
+                     (get 0)
+                     (:systems)
+                     (:engines))))))
 
 
 (deftest depletedShields
@@ -169,46 +169,46 @@
         enemyShip (:enemyShip sim-db)
         laserAttackInit [playerShip enemyShip :engines 40 :lasers]
         laserAttackFinal (-> laserAttackInit
-                           (events/newHP)
-                           (events/newShields)
-                           (events/newSystemHP))
-        missileAttackInit [playerShip enemyShip :engines 40 :missiles]
-        missileAttackFinal (-> missileAttackInit
                              (events/newHP)
                              (events/newShields)
-                             (events/newSystemHP))]
+                             (events/newSystemHP))
+        missileAttackInit [playerShip enemyShip :engines 40 :missiles]
+        missileAttackFinal (-> missileAttackInit
+                               (events/newHP)
+                               (events/newShields)
+                               (events/newSystemHP))]
     ;--------laser attack calculations--------
 
-    ;checks shields of defender   
+    ;checks shields of defender
     (is (= 0 (-> laserAttackFinal
-                (get 0)
-                (:shields))))
+                 (get 0)
+                 (:shields))))
     ;checks HP of defender
     (is (= 60 (-> laserAttackFinal
-                 (get 0)
-                 (:HP))))
+                  (get 0)
+                  (:HP))))
     ;checks targeted system of defender
     (is (= [1 1] (-> laserAttackFinal
-                   (get 0)
-                   (:systems)
-                   (:engines))))
+                     (get 0)
+                     (:systems)
+                     (:engines))))
 
 
     ;---------missile attack calculations------
 
-    ;checks shields of defender   
+    ;checks shields of defender
     (is (= 0 (-> missileAttackFinal
-               (get 0)
-               (:shields))))
+                 (get 0)
+                 (:shields))))
     ;checks HP of defender
     (is (= 60 (-> missileAttackFinal
-                (get 0)
-                (:HP))))
+                  (get 0)
+                  (:HP))))
     ;checks systems of defender
     (is (= [1 1] (-> missileAttackFinal
-                   (get 0)
-                   (:systems)
-                   (:engines))))))
+                     (get 0)
+                     (:systems)
+                     (:engines))))))
 
 
 
@@ -224,21 +224,21 @@
                     :shields 10
                     :ammo 5}
         newShip (-> [:engines testShip]
-                  (events/restoreHP)
-                  (events/restoreSystem)
-                  (get 1)
-                  (events/chargeShields randomAmount)
-                  (events/consumeAmmo))
+                    (events/restoreHP)
+                    (events/restoreSystem)
+                    (get 1)
+                    (events/chargeShields randomAmount)
+                    (events/consumeAmmo))
         newHP (:HP newShip)
         newAmmo (:ammo newShip)
         newShields (:shields newShip)
         newEngines (-> newShip :systems :engines)]
-        
+
 
     ;checking HP
     (is (= true (and (>= newHP 34)
                      (<= newHP 54))))
-    
+
     ;checking shields
     (is (= true (and (>= newShields 18)
                      (<= newShields 58))))
@@ -256,17 +256,17 @@
   (let [testShip {:systems {:lasers [1 1]}
                   :ammo 5}]
 
-  (is (= 5 (-> testShip
-             (events/refillAmmo 1)
-             (:ammo))))
+    (is (= 5 (-> testShip
+                (events/refillAmmo 1)
+                (:ammo))))
 
-  (is (= 6 (-> testShip
-             (events/refillAmmo 4)
-             (:ammo))))
+    (is (= 6 (-> testShip
+               (events/refillAmmo 4)
+               (:ammo))))
 
-  (is (= 4 (-> testShip
-             (events/consumeAmmo)
-             (:ammo))))))
+    (is (= 4 (-> testShip
+                (events/consumeAmmo)
+                (:ammo))))))
 
 
 
